ae2bf5052f0cdca9d6158b6c45f03375b7b1ae28
James Ball <42615415+jadball@users.noreply.github.com>
2024-05-23 10:21:10 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyDVC',
-    version='1.0',
+    version='0.0.3',
     packages=setuptools.find_packages(),
     url='https://github.com/jadball/pyDVC',
     license='BSD 3-Clause',
